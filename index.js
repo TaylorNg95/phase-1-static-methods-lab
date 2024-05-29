90eaa76de473794884da26589d6a22208679a672
Taylor Ng <taylormng95@gmail.com>
2024-05-29 10:33:19 -0400

    completes static methods lab

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string){
+    return string[0].toUpperCase() + string.slice(1)
+  }
+  static sanitize(string){
+    return string.replace(/[^A-Za-z0-9-' ]+/g, "")
+  }
+  static titleize(string){
+    const notCapital = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    let stringWords = string.split(' ')
+    stringWords = stringWords.map(element => {
+      if(notCapital.find(item => item === element) === undefined){
+        return element[0].toUpperCase() + element.slice(1)
+      } else {return element[0] + element.slice(1)}
+    })
+    let newString = stringWords.join(' ')
+    newString = newString[0].toUpperCase() + newString.slice(1)
+    return newString
+  }
 }
